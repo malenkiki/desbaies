089789dd503f198460771d7b343e6dbedc38b89c
Michel Petit <petit.michel@gmail.com>
2013-12-07 12:21:42 +0100

    Starts some API doc

--- src/Malenki/DesBaies/DesBaies.php
+++ src/Malenki/DesBaies/DesBaies.php
@@ -1,4 +1,26 @@
 <?php
+/*
+Copyright (c) 2013 Michel Petit <petit.michel@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 namespace Malenki\DesBaies;
 
@@ -9,6 +31,22 @@ use Malenki\DesBaies\Stack\Having as StackHaving;
 use Malenki\DesBaies\Stack\OrderBy as StackOrderBy;
 use Malenki\DesBaies\Stack\GroupBy as StackGroupBy;
 
+/**
+ * DesBaies writes for you SQL requests.
+ *
+ * With **DesBaies**, you can create SQL requests without SQL code, in PHP 
+ * only. This is great into complex request creation.
+ * 
+ * @property-read \Malenki\DesBaies\Stack\Select $select Select part
+ * @property-read \Malenki\DesBaies\Stack\From $from From part
+ * @property-read \Malenki\DesBaies\Stack\Where $where Where part
+ * @property-read \Malenki\DesBaies\Stack\Having $having Having part
+ * @property-read \Malenki\DesBaies\Stack\Order $order Order part
+ * @property-read \Malenki\DesBaies\Stack\Group $group Group part
+ *
+ * @author Michel Petit <petit.michel@gmail.com> 
+ * @license MIT
+ */
 class DesBaies
 {
     protected $select;
